fcb75f8138e28bc197a0c90bb965cb8fdda23c37
Andrew Lewis <nerf@judo.za.org>
2020-05-12 00:35:50 +0200

    Disable accounts that exceed limits

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE users (user text not null, max_bnc int default 1);
+CREATE TABLE users (user text not null, max_bnc int default 1, enabled default true);
 CREATE TABLE activity(user text not null, day date not null, num_bnc int default 0);
 CREATE UNIQUE INDEX users_user_idx on users(user);
 CREATE UNIQUE INDEX activity_user_day_idx ON activity(user, day);
