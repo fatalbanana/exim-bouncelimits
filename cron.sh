2ac8b6d5971f83b952151fd650c7e084c5d5c8a1
Andrew Lewis <nerf@judo.za.org>
2020-05-12 16:48:39 +0200

    Fix cronjob

--- cron.sh
+++ cron.sh
@@ -1,1 +1,1 @@
-sqlite3 /var/lib/bouncelimits/db.sqlite "delete from activity where day < date('now', '-7 day')"
+sqlite3 /var/lib/bouncelimits/db.sqlite "delete from activity where dtime < datetime('now', '-24 hours')"
